29433025ba6337848e9398169a2601696127042a
Adly Thebaud <adlythebaud@gmail.com>
2020-11-05 14:16:54 -0600

    returns unreversed body

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json())
+app.use(express.urlencoded())
+
+
+
+reverseString = (req, res, next) => {
+    req.headers['content-type'] = 'application/json';
+    var body = req.body;
+      
+    res.json({
+        data: body
+    });
+}
+
+app.get('/', (req, res) => {
+//   res.send('Hello World!')
+    res.json({data: "hello"})
+})
+
+app.post('/v1', reverseString)
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
